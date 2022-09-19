c3409691ba685d30a5ef7435b1c48d3ba9df2176
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-09-19 08:54:08 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Write("Enter M (beginning of the bucket): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter N (ending of the bucket): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m == 0 || n == 0)
+{
+    Console.WriteLine("Ooops...Entered numbers are incorrect");
+}
+while(m <= n)
+{
+    Console.Write(m + " ");
+    m++;
+}
